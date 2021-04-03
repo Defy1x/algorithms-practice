a5d70290cc0c98bb402d7f9b268a3864c0b59b2b
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-04-02 22:36:17 -0400

    solved sum array

---
+++ 05-sum-array (solved)/Unsolved/sum-array.js
@@ -0,0 +1,9 @@
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr){
+var sum = 0
+  for(let i = 0; i < arr.length; i++){
+  var sumArray = sum += arr[i];
+}
+  console.log(sumArray);
+};
