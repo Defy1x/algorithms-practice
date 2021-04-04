9da492dec5299bf70d5ab056411e1ea001010a79
Casandra Mcdonald <casandramcdonald@MacBook-Pro.local>
2021-04-04 15:33:48 -0400

    some solved

--- 14-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 14-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function(arr) {
+  let num1 = Math.max(...arr)
+  console.log(num1)
+
+  
+};
